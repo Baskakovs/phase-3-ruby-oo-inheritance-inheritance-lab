4cfd06c1a82598aa1d0146455ee59d8090b19350
Baskakovs <baskakovs@me.com>
2022-10-24 09:31:23 +0100

    commit tree 9f7e46e08687537e2b5921c41e9de08d02d71ffc

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,10 @@
-class Student 
-
+require_relative 'user.rb'
+class Student < User
+    attr_accessor :knowledge
+    def initialize()
+        @knowledge = []
+    end
+    def learn(value)
+        @knowledge << value
+    end
 end
